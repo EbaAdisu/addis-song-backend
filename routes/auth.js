2fa9741e597e8a55bd61a7ff2fb52ec2980d4f21
Eba Adisu <ebaadisu2@gmail.com>
2024-10-03 14:15:06 +0300

    Add GET route for Auth

--- routes/auth.js
+++ routes/auth.js
@@ -5,5 +5,8 @@ const { signin, signup } = require('../controllers/auth')
 
 router.route('/signin').post(signin)
 router.route('/signup').post(signup)
+router.route('/').get((req, res) => {
+    res.send('Auth route')
+})
 
 module.exports = router
