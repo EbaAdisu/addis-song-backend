c6e84f80331a16e520951be2f3c6924b91c026ff
Eba Adisu <ebaadisu2@gmail.com>
2024-10-03 13:43:36 +0300

    Refactor app.js and add Netlify configuration

--- app.js
+++ app.js
@@ -1,43 +1,29 @@
-require('express-async-errors')
-require('dotenv').config()
-const cors = require('cors')
-
 const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
 
 const app = express()
 
-// Import DB
+// Import DB and Routers
 const connectDB = require('./db/connect')
-
-// import Routers
 const authRouter = require('./routes/auth')
 const songRouter = require('./routes/song')
 
 // Middleware
-
 const authenticationMiddleware = require('./middlewares/authentication')
 const errorHandler = require('./middlewares/error-handler')
 const notFound = require('./middlewares/not-found')
 
-// package
+// Package middlewares
 app.use(express.json())
 app.use(cors())
 
 // Routes
-
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/song', authenticationMiddleware, songRouter)
-// Middleware
+
+// Error handlers
 app.use(errorHandler)
 app.use(notFound)
 
-const port = process.env.PORT
-app.listen(port, async () => {
-    try {
-        await connectDB(process.env.MONGO_URI)
-        console.log('Connected to the database successfully')
-        console.log(`Server is listening on port ${port}`)
-    } catch (error) {
-        console.error(error)
-    }
-})
+module.exports = app
